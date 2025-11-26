12c53a05970d0ebacd456368926f686b32683969
amurdeshwar <akash.murdeshwar@scientificgames.com>
2025-11-26 14:13:41 +0530

    Delete Release-1/Tables.sql

--- Release-1/Tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE dbo.Test_script (
-    EmpId INT IDENTITY(1,1) PRIMARY KEY,
-    EmpName VARCHAR(100),
-    Salary DECIMAL(10,2)
-);
-GO
-
-ALTER TABLE dbo.Test_script ADD Salary DECIMAL(10,2);

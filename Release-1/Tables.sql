6e1a6d65f4cc6896dd68378306070e4ad8077a5b
amurdeshwar <akash.murdeshwar@scientificgames.com>
2025-11-26 14:09:30 +0530

    Update Tables.sql

--- Release-1/Tables.sql
+++ Release-1/Tables.sql
@@ -1,5 +1,3 @@
--- ScriptID: 1
--- ScriptName: Create Test_script table
 CREATE TABLE dbo.Test_script (
     EmpId INT IDENTITY(1,1) PRIMARY KEY,
     EmpName VARCHAR(100),
@@ -7,6 +5,4 @@ CREATE TABLE dbo.Test_script (
 );
 GO
 
--- ScriptID: 2
--- ScriptName: Add Salary Column
-ALTER TABLE dbo.Test_script ADD Salary DECIMAL(10,2)
+ALTER TABLE dbo.Test_script ADD Salary DECIMAL(10,2);

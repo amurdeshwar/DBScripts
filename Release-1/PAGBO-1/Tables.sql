4baf3580bb311c6d301d1ff3b8c108a4f098973f
amurdeshwar <akash.murdeshwar@scientificgames.com>
2025-11-26 17:35:06 +0530

    Update Tables.sql

--- Release-1/PAGBO-1/Tables.sql
+++ Release-1/PAGBO-1/Tables.sql
@@ -3,11 +3,8 @@ CREATE TABLE dbo.Test_script (
     EmpName VARCHAR(100),
     Salary DECIMAL(10,2)
 );
-GO
 
 ALTER TABLE dbo.Test_script ADD Age INT;
-
---ALTER TABLE dbo.Test_script ADD Age INT;
 
 CREATE TABLE dbo.Test_script12345 (
     EmpId INT IDENTITY(1,1) PRIMARY KEY,
